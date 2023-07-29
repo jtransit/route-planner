89a3e4a3c3cc051bb6663ba9c1ab40c7e48d4432
Webster Dave Bontilao <websterbontilao@gmail.com>
2023-07-29 12:47:18 +0800

    Added initial routeControl details

---
+++ src/app/components/RoutingControl.ts
@@ -0,0 +1,17 @@
+import L from 'leaflet';
+import 'leaflet-routing-machine';
+
+export const routeControl = L.Routing.control({
+  waypoints:[],
+  lineOptions: {
+    styles: [{color: "#FF2D00", weight: 5}],
+    extendToWaypoints: true,
+    missingRouteTolerance: 0
+  },
+  show: false,
+  addWaypoints: false,
+  routeWhileDragging: true,
+  fitSelectedRoutes: true,
+  showAlternatives: false,
+  containerClassName: 'display-none'
+});
