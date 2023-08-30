b31b8d7e753237e8c588632f79b8fed2bd351a47
Ivan Apos <aposivanrodger@gmail.com>
2023-08-30 14:55:14 +0800

    Removed unnecessary lines

--- src/app/components/menu/navigation-menu.tsx
+++ src/app/components/menu/navigation-menu.tsx
@@ -8,12 +8,10 @@ const NavigationMenu = () => {
   const _to = to?.toString() ?? '';
 
   return (
-    <>
-      <div className='absolute z-[400] w-[25rem] h-auto border-2 border-solid border-gray-400 rounded p-[0.3rem] bg-white top-[1rem] left-[1rem]'>
-        <div className='py-1'>Where are you? {_from}</div>
-        <div className='py-1'>Where do you want to go? {_to}</div>
-      </div>
-    </>
+    <div className='absolute z-[400] w-[25rem] h-auto border-2 border-solid border-gray-400 rounded p-[0.3rem] bg-white top-[1rem] left-[1rem]'>
+      <div className='py-1'>Where are you? {_from}</div>
+      <div className='py-1'>Where do you want to go? {_to}</div>
+    </div>
   );
 };
 
