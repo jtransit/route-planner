3b06a7d82f6bc855c13e7c0eb6d3b4b874a6aaba
Ivan Apos <aposivanrodger@gmail.com>
2023-09-01 07:10:48 +0800

    Added show/hide drawer app context

--- src/app/hooks/use-app-context-state.ts
+++ src/app/hooks/use-app-context-state.ts
@@ -4,13 +4,21 @@ import { AppContextProps } from '@app-types/app-context';
 const useAppContextState: () => AppContextProps = () => {
   const [isLoading, setIsLoading] = useState(false);
 
+  const [showDrawer, setShowDrawer] = useState(false);
+
   const handleLoading = (v: boolean) => {
     setIsLoading(v);
   };
 
+  const handleShowDrawer = (v: boolean) => {
+    setShowDrawer(v);
+  };
+
   return {
     isLoading,
+    showDrawer,
     handleLoading,
+    handleShowDrawer,
   };
 };
 
--- src/app/types/app-context.ts
+++ src/app/types/app-context.ts
@@ -2,12 +2,16 @@ import { ReactNode } from 'react';
 
 export interface AppContextProps {
   isLoading: boolean;
+  showDrawer: boolean;
   handleLoading: (value: boolean) => void;
+  handleShowDrawer: (value: boolean) => void;
 }
 
 export const defaultAppContext: AppContextProps = {
   isLoading: false,
+  showDrawer: false,
   handleLoading: () => {},
+  handleShowDrawer: () => {},
 };
 
 export interface AppContextProviderProps {
