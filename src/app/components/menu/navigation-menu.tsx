c80d9f55d372d987fabcf1e49e06b7a87bb9df25
Ivan Apos <aposivanrodger@gmail.com>
2023-09-20 11:32:36 +0800

    refactor: navigation menu

--- src/app/components/menu/navigation-menu.tsx
+++ src/app/components/menu/navigation-menu.tsx
@@ -1,11 +1,5 @@
 import { useMemo } from 'react';
-import {
-  Box,
-  Divider,
-  Autocomplete,
-  TextField,
-  CircularProgress,
-} from '@mui/material';
+import { Box, Divider } from '@mui/material';
 import TripOriginIcon from '@mui/icons-material/TripOrigin';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import SwapVertIcon from '@mui/icons-material/SwapVert';
@@ -14,59 +8,12 @@ import { useSpring, animated } from '@react-spring/web';
 import { useAppContext } from '@contexts/app-context';
 import { useMapContext } from '@contexts/map-context';
 import _styles from './styles';
-import { LngLat } from '@app-types/directions';
+import AutoCompleteComponent from './autocomplete';
 
 const styles = _styles.nav;
 
-interface Option {
-  label: string;
-  center: LngLat;
-}
-
-interface AutoCompleteComponentProps {
-  isLoading: boolean;
-  value: Option;
-  options: Option[];
-  changeHandler: (label?: Option['label'], center?: Option['center']) => void;
-}
-
-const AutoCompleteComponent = ({
-  isLoading,
-  options,
-  value,
-  changeHandler,
-}: AutoCompleteComponentProps) => {
-  return (
-    <Autocomplete
-      options={options}
-      sx={styles.input}
-      onChange={(e, v) => {
-        changeHandler(v?.label ?? '', v?.center);
-      }}
-      value={value}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          onChange={(e) => changeHandler((e.target as HTMLInputElement).value)}
-          InputProps={{
-            ...params.InputProps,
-            endAdornment: (
-              <>
-                {isLoading ? (
-                  <CircularProgress color='inherit' size={20} />
-                ) : null}
-                {params.InputProps.endAdornment}
-              </>
-            ),
-          }}
-        />
-      )}
-    />
-  );
-};
-
 const NavigationMenu = () => {
-  const { showDrawer } = useAppContext();
+  const { showDrawer, showNavigationMenu } = useAppContext();
 
   const {
     directions: {
@@ -79,6 +26,7 @@ const NavigationMenu = () => {
 
   const props = useSpring({
     left: showDrawer ? styles.drawerOpen.left : styles.drawerClose.left,
+    opacity: showNavigationMenu ? 1 : 0,
   });
 
   const options = useMemo(
@@ -102,9 +50,10 @@ const NavigationMenu = () => {
           <AutoCompleteComponent
             isLoading={isLoadingSearch}
             options={options}
+            placeholder='Directions from'
             value={{
               center: [from.latLng?.lng ?? 0, from.latLng?.lat ?? 0],
-              label: from?.address ?? '',
+              label: from.address ?? '',
             }}
             changeHandler={handleChangeFrom}
           />
@@ -120,6 +69,7 @@ const NavigationMenu = () => {
           <AutoCompleteComponent
             isLoading={isLoadingSearch}
             options={options}
+            placeholder='Directions to'
             value={{
               center: [to.latLng?.lng ?? 0, to.latLng?.lat ?? 0],
               label: to.address ?? '',
